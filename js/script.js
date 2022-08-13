7c9890eaa70c19573d25c0dc6d07992867e0258b
RaizaCirne <raizabraz93@gmail.com>
2022-08-13 16:50:04 -0300

    js para button de pagamento

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 document.getElementById("pagamento").addEventListener("click", validaFormulario)
 
+function validaFormulario(){
+    if (document.getElementById("pagamento").value != ""){
+        alert ("Prontinho! Você será direcionado para a aba de pagamentos.")
+        }
+}
+
+
+
+
