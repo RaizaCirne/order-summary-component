214773074790f69734d05c0f37cde76a1ee87505
RaizaCirne <raizabraz93@gmail.com>
2022-08-14 12:23:21 -0300

    Página de pagamento finalizada

---
+++ js/pagamento.js
@@ -0,0 +1,13 @@
+document.getElementById("submit").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+    if (document.getElementById("email").value != "" && 
+        document.getElementById ("dados").value != "" &&
+        document.getElementById("data").value != "" &&
+        document.getElementById("validade").value != ""){
+        alert ("Prontinho! Agora você já pode ter acesso a todas as playlists de música brasileira.")
+        }else{
+        
+}
+
+
